0720f6621fdcae54389424d0baff5b91641c9599
Jianjun Luo <jluo@AIR-WORLDWIDE.COM>
2016-02-26 00:02:27 -0500

    create plot 1 of project 1

---
+++ plot1.R
@@ -0,0 +1,17 @@
+# Exploratory Data Analysis
+# Plot 1
+
+rm(list=ls())
+
+# Load and process data
+rawData<-read.table("household_power_consumption.txt", header=TRUE, sep=";",colClasses="character",na.strings = "?")
+rawData$Date<-as.Date(rawData$Date, "%d/%m/%Y")
+subData<-subset(rawData, rawData$Date %in% as.Date(c("2007-02-01","2007-02-02")))
+subData$Global_active_power <- as.numeric(subData$Global_active_power)
+subData$Global_reactive_power <- as.numeric(subData$Global_reactive_power)
+subData$Voltage <- as.numeric(subData$Voltage)
+subData$Global_intensity <- as.numeric(subData$Global_intensity)
+subData$Sub_metering_1 <- as.numeric(subData$Sub_metering_1)
+subData$Sub_metering_2 <- as.numeric(subData$Sub_metering_2)
+subData$Sub_metering_3 <- as.numeric(subData$Sub_metering_3)
+
